ca1c5891094ecf419f3b650ea3ea84bd055bc97a
sukhpreetsvirdi <sukhpreetsvirdi@gmail.com>
2025-07-30 22:06:55 +0530

    commiting updated map and structure

--- script.js
+++ script.js
@@ -96,5 +96,26 @@ window.addEventListener("scroll", function () {
 });
 
 
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+  let scrollY = window.scrollY + 150;
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    const sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === `#${sectionId}`) {
+          link.classList.add("active");
+        }
+      });
+    }
+  });
+});
 
 
